dd5c365026ae1edc025c79b49e4458aeaf1f2673
676009616 <676009616@qq.com>
2019-05-08 17:47:41 +0800

    init

---
+++ app.js
@@ -0,0 +1,32 @@
+const Hapi = require('hapi')
+const config = require('./config')
+const routesHelloHapi = require('./routes/hello-hapi')
+const routesShops = require('./routes/shops')
+const routesOrder = require('./routes/orders')
+// 引入自定义的 hapi-swagger 插件配置
+const pluginHapiSwagger = require('./plugins/hapi-swagger')
+
+const server = new Hapi.Server();
+// 配置服务器启动 host 与端口
+server.connection({
+  port: config.port,
+  host: config.host
+});
+
+const init = async () => {
+  await server.register([
+    // 为系统使用 hapi-swagger
+    ...pluginHapiSwagger
+  ])
+  server.route([
+    // 创建一个简单的 hello hapi 接口
+    ...routesHelloHapi,
+    ...routesShops,
+    ...routesOrder
+  ]);
+  // 启动服务
+  await server.start();
+  console.log(`Server running ar: ${server.info.uri}`);
+};
+
+init();
---
+++ config/index.js
@@ -0,0 +1,9 @@
+// config/index.js
+require('env2')('./.env')
+
+const { env } = process;
+
+module.exports = {
+  host: env.HOST,
+  port: env.PORT,  
+}
